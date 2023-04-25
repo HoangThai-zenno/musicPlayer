97d9ddfb249c407aa06e8c5ac806a3776ca96df2
Hoang_Thai <tlee692001@gmail.com>
2023-04-26 01:34:36 +0700

    add songs, build next, prev, random song

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,29 +7,51 @@ const cd = $(".cd")
 const playbtn = $(".btn-toggle-play")
 const player = $(".player")
 const progress = $("#progress")
+const btnNext = $(".btn-next")
+const btnPrev = $(".btn-prev")
+const btnRandom = $(".btn-random")
 const app = {
     currentIndex: 0,
     isPlaying: false,
+    isRandom: false,
     songs: [
 
         {
             name: "Anh không thể",
             singer: "Mono",
             path: "./assets/music/anhkhongthe_Mono.mp3",
-            image: "./assets/images/MONO1.jpg"
+            image: "./assets/images/anh-khong-the.jpg"
         },
         {
             name: "Em là",
             singer: "Mono",
             path: "./assets/music/emla_Mono.mp3",
-            image: "./assets/images/singer.jpg"
+            image: "./assets/images/Emla.jpg"
         },
         {
             name: "Waitting For You",
             singer: "Mono",
             path: "./assets/music/waittingforyou_Mono.mp3",
             image: "./assets/images/singer.jpg"
-        }
+        },
+        {
+            name: "Độ tộc 2",
+            singer: "MixiGaming",
+            path: "./assets/music/do_toc_2_DoMiXi.mp3",
+            image: "./assets/images/dotoc.jpg"
+        },
+        {
+            name: "Lửng lơ",
+            singer: "Masew x Bray",
+            path: "./assets/music/lung_lo_Masew_Bray.mp3",
+            image: "./assets/images/lunglo.jpg"
+        },
+        {
+            name: "Walk on da street",
+            singer: "16Typh x 16Brt",
+            path: "./assets/music/walk_on_da_street_16_typh_x_16_brt.mp3",
+            image: "./assets/images/walkondastreet.jpg"
+        },        
     ],
     render: function () {
         const htmls = this.songs.map(song => {
@@ -101,7 +123,51 @@ const app = {
             const seekTime = audio.duration / 100 * e.target.value
             audio.currentTime = seekTime
         }
+        btnNext.onclick = function () {
+            if(_this.isRandom){
+                _this.randomSong()
+            }else{
 
+                _this.nextSong()
+            }
+            audio.play()
+        },
+        btnPrev.onclick = function () {
+            if(_this.isRandom){
+                _this.randomSong()
+            }else{
+
+                _this.prevSong()
+            }
+            audio.play()
+        },
+        btnRandom.onclick = function () {
+            _this.isRandom = !_this.isRandom
+            btnRandom.classList.toggle("active")
+        }
+        
+    },
+    nextSong: function () {
+        this.currentIndex++;
+        if(this.currentIndex >= this.songs.length){
+            this.currentIndex = 0
+        }
+        this.loadCurrentSong()
+    },
+    prevSong: function () {
+        this.currentIndex--;
+        if(this.currentIndex < 0){
+            this.currentIndex = this.songs.length - 1
+        }
+        this.loadCurrentSong()
+    },
+    randomSong: function () {
+        let newIndex
+        do {
+            newIndex = Math.floor(Math.random() * this.songs.length)
+        } while (newIndex === this.currentIndex)
+        this.currentIndex = newIndex
+        this.loadCurrentSong()
     },
     loadCurrentSong: function () {
 
